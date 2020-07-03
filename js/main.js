6a1d79f371bbfa3c9741c486aa8a9e8306d479e3
jinglad <57634584+jinglad@users.noreply.github.com>
2020-07-04 02:49:05 +0600

    responsive and menu done

--- js/main.js
+++ js/main.js
@@ -7,6 +7,23 @@ $(document).ready(function(){
             $("nav").removeClass("sticky");
         }
     });
+
+    $(".menu ul li a").click(function(){
+        $(".menu ul li a").removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $("nav .logo a img").click(function(){
+        $("nav .logo a img").removeClass("active");
+        $(".menu ul li:first-child a").addClass("active");
+    });
     
     var mixer = mixitup('.mixi');
 });
+
+function openNav(){
+    document.getElementById("myNav").style.width = "100%";
+}
+function closenav(){
+    document.getElementById("myNav").style.width = "0%";
+}
